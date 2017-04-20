2b12c2fc9873a463aa5e27f42da7681660d70151
Keilani Burak <Keilani@Keilanis-MacBook-Pro.local>
2017-04-19 22:34:15 -0700

    hehe

--- collegechances.js
+++ collegechances.js
@@ -1,12 +1,24 @@
 function harvard() {
-        if ((document.getElementById('screen1').value >= 4.5) && (document.getElementById('screen2').value == 800) && (document.getElementById('screen3').value >= 800) && (document.getElementById('screen4').value >= 35) && (document.getElementById('screen5').value >= 35) && (document.getElementById('screen6').value >= 35)) {
+        if ((document.getElementById('screen1').value >= 4.5) && (document.getElementById('screen2').value == 800) && (document.getElementById('screen3').value == 800) && (document.getElementById('screen4').value >= 35) && (document.getElementById('screen5').value >= 35) && (document.getElementById('screen6').value >= 35)) {
         return "Top applicant, but Harvard is VERY competitive. ";
     }
     else if ((document.getElementById('screen1').value >= 4.1) && (document.getElementById('screen2').value >= 700) && (document.getElementById('screen3').value >= 700) && (document.getElementById('screen4').value >= 32) && (document.getElementById('screen5').value >= 31) && (document.getElementById('screen6').value >= 33)) {
         return "Would be sufficient at another school, but won't be enough.";
     }
     else {
-        return "Little to know chance. Try an easier school.";
+        return "Little to no chance. Try an easier school.";
+    }
+}
+
+function berkeley() {
+        if ((document.getElementById('screen1').value >= 4.39) && (document.getElementById('screen2').value >= 740) && (document.getElementById('screen3').value >= 770) && (document.getElementById('screen4').value >= 34) && (document.getElementById('screen5').value >= 35) && (document.getElementById('screen6').value >= 35)) {
+        return "Top applicant, but Cal is highly competitive. ";
+    }
+    else if ((document.getElementById('screen1').value >= 4.0) && (document.getElementById('screen2').value >= 610) && (document.getElementById('screen3').value >= 640) && (document.getElementById('screen4').value >= 30) && (document.getElementById('screen5').value >= 29) && (document.getElementById('screen6').value >= 31)) {
+        return "You did well, but don't get your hopes up. ";
+    }
+    else {
+        return "Would be sufficient at another school, but won't be enough.";
     }
 }
 
@@ -34,4 +46,15 @@ function oregon() {
     }
 }
 
+function hawaii() {
+        if ((document.getElementById('screen1').value >= 3.5) && (document.getElementById('screen2').value >= 580) && (document.getElementById('screen3').value >= 610) && (document.getElementById('screen4').value >= 25) && (document.getElementById('screen5').value >= 26) && (document.getElementById('screen6').value >= 25)) {
+        return "High Chances! ";
+    }
+    else if ((document.getElementById('screen1').value >= 3.0) && (document.getElementById('screen2').value >= 480) && (document.getElementById('screen3').value >= 490) && (document.getElementById('screen4').value >= 20) && (document.getElementById('screen5').value >= 20) && (document.getElementById('screen6').value >= 19)) {
+        return "Could go either way.";
+    }
+    else {
+        return "Low chances. Try an easier school.";
+    }
+}
 
