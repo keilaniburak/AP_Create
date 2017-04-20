618bdf907f03e13bbe6c76e4b9732b9c42f99196
Keilani Burak <Keilani@Keilanis-MacBook-Pro.local>
2017-04-19 21:45:23 -0700

    Finally getting shit done

--- collegechances.js
+++ collegechances.js
@@ -1,3 +1,27 @@
+function harvard() {
+        if ((document.getElementById('screen1').value >= 4.5) && (document.getElementById('screen2').value == 800) && (document.getElementById('screen3').value >= 800) && (document.getElementById('screen4').value >= 35) && (document.getElementById('screen5').value >= 35) && (document.getElementById('screen6').value >= 35)) {
+        return "Top applicant, but Harvard is VERY competitive. ";
+    }
+    else if ((document.getElementById('screen1').value >= 4.1) && (document.getElementById('screen2').value >= 700) && (document.getElementById('screen3').value >= 700) && (document.getElementById('screen4').value >= 32) && (document.getElementById('screen5').value >= 31) && (document.getElementById('screen6').value >= 33)) {
+        return "Would be sufficient at another school, but won't be enough.";
+    }
+    else {
+        return "Little to know chance. Try an easier school.";
+    }
+}
+
+function washington() {
+    if ((document.getElementById('screen1').value >= 3.7) && (document.getElementById('screen2').value >= 660) && (document.getElementById('screen3').value >= 710) && (document.getElementById('screen4').value >= 31) && (document.getElementById('screen5').value >= 32) && (document.getElementById('screen6').value >= 32)) {
+        return "High Chances! ";
+    }
+    else if ((document.getElementById('screen1').value >= 3.4) && (document.getElementById('screen2').value >= 540) && (document.getElementById('screen3').value >= 610) && (document.getElementById('screen4').value >= 26) && (document.getElementById('screen5').value >= 26) && (document.getElementById('screen6').value >= 25)) {
+        return "Could go either way, make sure to have a safety.";
+    }
+    else {
+        return "Low chances. Try an easier school.";
+    }
+}
+
 function oregon() {
     if ((document.getElementById('screen1').value >= 3.6) && (document.getElementById('screen2').value >= 620) && (document.getElementById('screen3').value >= 610) && (document.getElementById('screen4').value >= 27) && (document.getElementById('screen5').value >= 27) && (document.getElementById('screen6').value >= 28)) {
         return "High Chances! ";
@@ -10,14 +34,4 @@ function oregon() {
     }
 }
 
-function washington() {
-    if ((document.getElementById('screen1').value >= 3.7) && (document.getElementById('screen2').value >= 660) && (document.getElementById('screen3').value >= 710) && (document.getElementById('screen4').value >= 31) && (document.getElementById('screen5').value >= 32) && (document.getElementById('screen6').value >= 32)) {
-        return "High Chances! ";
-    }
-    else if ((document.getElementById('screen1').value >= 3.4) && (document.getElementById('screen2').value >= 540) && (document.getElementById('screen3').value >= 710) && (document.getElementById('screen4').value >= 26) && (document.getElementById('screen5').value >= 26) && (document.getElementById('screen6').value >= 25)) {
-        return "Could go either way.";
-    }
-    else {
-        return "Low chances. Try an easier school.";
-    }
-}
+
